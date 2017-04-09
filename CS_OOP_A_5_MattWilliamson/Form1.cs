9d27df415c3ff8cc2c617f664cfcc989fcbed27d
Williamson-Matt <W0284910@nscc.ca>
2017-04-09 17:50:49 -0300

    making the project to ensure the repo works properly

--- CS_OOP_A_5_MattWilliamson/Form1.cs
+++ CS_OOP_A_5_MattWilliamson/Form1.cs
@@ -16,5 +16,10 @@ namespace CS_OOP_A_5_MattWilliamson
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
